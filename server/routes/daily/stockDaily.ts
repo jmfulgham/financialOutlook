fd4516e431946a498580a1bea7aec81da257cf9a
Mone't Fulgham <monetfulgham@gmail.com>
2024-02-21 20:31:25 -0500

    Change API endpoints

--- server/routes/daily/stockDaily.ts
+++ server/routes/daily/stockDaily.ts
@@ -1,19 +1,17 @@
 import express, {Request, Response, Router} from "express";
-import 'isomorphic-fetch';
 import dotenv from "dotenv";
 dotenv.config();
 
 const dailyStockRouter: Router = express.Router();
 
 
-dailyStockRouter.get("/api/daily/stock/:stockTicker", async (req: Request, res: Response): Promise<void> => {
+dailyStockRouter.get("/api/daily/stock/:stockTicker", async (req: Request, res: Response) => {
     const {stockTicker} = req.params;
     try {
         const response = await fetch(
-            `${process.env.ALPHA_VANTAGE_URL}TIME_SERIES_DAILY&symbol=${stockTicker}&apikey=${process.env.ALPHA_VANTAGE_API_KEY}`,
+            `${process.env.POLYGON_URL}/v2/aggs/ticker/${stockTicker}/prev?adjusted=true&apiKey=${process.env.POLYGON_API_KEY}`,
         );
-        res.send(response.json());
-        return;
+        res.send(await response.json());
     } catch (e: any) {
         console.error(e);
         throw new Error("Could not get daily stock information", e);
--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,14 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
-import {postgresDb} from "./config/database.js";
+import cors from 'cors';
+// import {postgresDb} from "./config/database.js";
 import dailyStockRouter from "./routes/daily/stockDaily.js";
 import weeklyStatsRouter from "./routes/weekly.js";
 import dailyCryptoRouter from "./routes/daily/cryptoDaily.js";
 
 dotenv.config();
 const app: Express = express();
+app.use(cors())
 const port = process.env.PORT || 8080;
 
 const router = express.Router();
@@ -22,13 +24,13 @@ app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
-(async (): Promise<void> => {
-    postgresDb.connect(function (err) {
-        if (err)
-            throw err;
-        console.log("DB is connected!");
-    })
-    const {rows} = await postgresDb.query('SELECT $1 AS food', ["pizza"])
-    console.log(rows)
-})()
+// (async (): Promise<void> => {
+//     postgresDb.connect(function (err) {
+//         if (err)
+//             throw err;
+//         console.log("DB is connected!");
+//     })
+//     const {rows} = await postgresDb.query('SELECT $1 AS food', ["pizza"])
+//     console.log(rows)
+// })()
 
