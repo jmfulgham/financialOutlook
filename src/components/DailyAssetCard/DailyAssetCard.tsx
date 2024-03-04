772dc0ee0342793fcbf16209c445625ba4ce31dc
Mone't Fulgham <monetfulgham@gmail.com>
2024-03-04 18:22:22 -0500

    Update layout and card styles/props

--- src/components/DailyAssetCard/DailyAssetCard.tsx
+++ src/components/DailyAssetCard/DailyAssetCard.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState} from "react";
 import "./styles.css"
 import {DailyCloseDetails, useGetDailyStockCloseInfo} from "../../utils/dailies";
 import "../../utils/dailyStock.json";
-import { ResponsiveLine } from '@nivo/line'
+// import { ResponsiveLine } from '@nivo/line'
 
-
-const DailyAssetCard = () => {
+interface DailyAssetCardProps {
+    ticker: string
+}
+const DailyAssetCard = ({ticker}: DailyAssetCardProps) => {
     const date = new Date().toISOString().slice(0, 10);
 
 
@@ -13,7 +15,7 @@ const DailyAssetCard = () => {
     const [error, setError] = useState(false)
     useEffect(() => {
         const fetchData = async () => {
-            const resp = await useGetDailyStockCloseInfo("aapl");
+            const resp = await useGetDailyStockCloseInfo(ticker);
             if (resp.error) {
                 setError(true)
                 return
@@ -23,71 +25,14 @@ const DailyAssetCard = () => {
 
         fetchData()
     })
-    const createGraph  = () =>(
-        <ResponsiveLine
-            curve={"monotoneX"}
-            data={graphData}
-            enablePoints={false}
-            margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-            xScale={{ type: 'point' }}
-            yScale={{
-                type: 'linear',
-                min: 'auto',
-                max: 'auto',
-                stacked: true,
-                reverse: false
-            }}
-            yFormat=" >-.2f"
-            axisTop={null}
-            axisRight={null}
-            axisBottom={{
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legend: 'transportation',
-                legendOffset: 36,
-                legendPosition: 'middle'
-            }}
-            axisLeft={{
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legend: 'count',
-                legendOffset: -40,
-                legendPosition: 'middle'
-            }}
-            useMesh={true}
-        />
-    )
-
-    const graphData = [
-        {
-            "id": "aapl",
-            "color": "hsl(306, 70%, 50%)",
-            "data": [
-                {
-                    "x": "test",
-                    "y": 187
-                },
-                {
-                    "x": "test1",
-                    "y": 300
-                },
-                {
-                    "x": "test2",
-                    "y": 245
-                },
-            ]
-        }
-    ]
 
     return (
         <div className={"daily-asset-card-container"}>
             {stockData && <p className={"company-name"}>{stockData.ticker}</p>}
             {stockData && <p className={"price"}>${stockData.closePrice}</p>}
             <div className={"graph-container"}>
-                {createGraph()}
-        </div>
+            {/*    graph goes here*/}
+            </div>
         </div>
     )
 }
--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import './styles.css';
 import DailyAssetCard from "../DailyAssetCard/DailyAssetCard";
 
+const tickers = ['aapl', 'ibm', 'nvda']
 const Dashboard= ()=> {
     return(<div className={"dashboard"}>
         <h1>Dashboard</h1>
         <div className={"cards-container"}>
-            <DailyAssetCard/>
+            {tickers.map((ticker) =>  <DailyAssetCard ticker={ticker}/>)}
         </div>
 
     </div>)
