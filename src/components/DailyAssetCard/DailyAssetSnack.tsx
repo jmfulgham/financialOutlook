9d7c3fd526304d2282ed08864969610f8f0bc30d
Mone't Fulgham <monetfulgham@gmail.com>
2024-06-19 13:54:48 -0500

    Update Git ignore

--- src/components/DailyAssetCard/DailyAssetSnack.tsx
+++ src/components/DailyAssetCard/DailyAssetSnack.tsx
@@ -1,42 +1,42 @@
-import React, { useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./styles.css"
 import {DailyCloseDetails, useGetDailyStockCloseInfo} from "../../utils/dailies";
 import "../../utils/dailyStock.json";
-// import { ResponsiveLine } from '@nivo/line'
+import { Line } from 'react-chartjs-2';
+
 
 interface DailyAssetCardProps {
     ticker: string
 }
-const DailyAssetCard = ({ticker}: DailyAssetCardProps) => {
 
+const DailyAsset = ({ticker}: DailyAssetCardProps) => {
+//TODO export the graph
     const [stockData, setStockData] = useState<DailyCloseDetails | undefined>()
     const [error, setError] = useState(false)
+
     useEffect(() => {
         const fetchData = async () => {
-            const resp = await useGetDailyStockCloseInfo(ticker);
-            if (resp.error) {
+            const dailyClose = await useGetDailyStockCloseInfo(ticker);
+            if (dailyClose.error) {
                 setError(true)
                 return
             }
-            setStockData(resp)
+            setStockData(dailyClose)
         }
 
         fetchData()
     }, [ticker])
 
-    if (error){
-        return <div>Error retrieving stock details, please try again</div>
-    }
-
     return (
         <div className={"daily-asset-card-container"}>
-            {stockData && <p className={"company-name"}>{stockData.ticker}</p>}
-            {stockData && <p className={"price"}>${stockData.closePrice}</p>}
+            {error && <div>Error retrieving stock details, please try again</div>}
+            {stockData && <p className={"company-ticker"}>{stockData.ticker}</p>}
+            {stockData && <p className={"price"}>Close: ${stockData.closePrice}</p>}
             <div className={"graph-container"}>
-            {/*    graph goes here*/}
+
             </div>
         </div>
     )
 }
 
-export default DailyAssetCard
+export default DailyAsset
