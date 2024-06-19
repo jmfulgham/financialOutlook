9d5c4b94e970fa46efee5880bf1929768d910c26
Mone't Fulgham <monetfulgham@gmail.com>
2024-06-19 13:59:45 -0500

    Fix daily stock details API requests

--- src/utils/dailies.ts
+++ src/utils/dailies.ts
@@ -1,16 +1,17 @@
 
 export interface DailyCloseDetails {
-    ticker?: string,
-    closePrice?: number,
+    symbol: string,
+    close: number,
     error?: unknown,
-    message?: unknown
+    from: string,
+    open: number,
+    afterHours: number
 }
 export const useGetDailyStockCloseInfo = async (stockTicker: string) => {
     //TODO update this call w env vars
     try {
         const response = await fetch(`http://localhost:8080/api/daily/stock/${stockTicker.toUpperCase()}`);
-        const results = await response.json()
-        return {ticker: results.results[0].T, closePrice: results.results[0].c}
+        return await response.json()
     } catch(e){
       console.log(e)
         return {error: e, message: "Error retrieving daily close details"}
